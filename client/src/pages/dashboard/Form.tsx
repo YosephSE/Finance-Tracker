25b0346ea67a5430c676b651e0fef86d126b51fa
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-17 00:06:51 +0300

    frontend form added

--- client/src/pages/dashboard/Form.tsx
+++ client/src/pages/dashboard/Form.tsx
@@ -1,11 +1,96 @@
-
-
-const Form = () => {
-  return (
-    <div>
-      Form
-    </div>
-  )
+import { useState } from "react";
+import { useUser } from "@clerk/clerk-react";
+interface RecordType {
+  userId: string | undefined;
+  date: number;
+  desc: string;
+  amount: number;
+  category: string;
+  paymentMethod: string;
 }
+const Form = () => {
+  const [amount, setAmount] = useState<string>("");
+  const [desc, setDesc] = useState<string>("");
+  const [category, setCategory] = useState<string>("");
+  const [paymentMethod, setPaymentMethod] = useState<string>("");
+  const { user } = useUser();
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newRecord: RecordType = {
+      userId: user?.id,
+      date: new Date().getTime(),
+      desc,
+      amount: parseInt(amount),
+      category,
+      paymentMethod,
+    };
+    console.log(newRecord);
+    // addRecord(newRecord)
+    setAmount("");
+    setDesc("");
+    setCategory("");
+    setPaymentMethod("");
+  };
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label>Description:</label>
+          <input
+            type="text"
+            required
+            className="input"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
+        </div>
+        <div className="form-field">
+          <label>Amount:</label>
+          <input
+            type="number"
+            required
+            className="input"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <div className="form-field">
+          <label>Category:</label>
+          <select
+            required
+            className="input"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="">Select a Category</option>
+            <option value="Food">Food</option>
+            <option value="Rent">Rent</option>
+            <option value="Salary">Salary</option>
+            <option value="Utilities">Utilities</option>
+            <option value="Entertainment">Entertainment</option>
+            <option value="Other">Other</option>
+          </select>
+        </div>
+        <div className="form-field">
+          <label>Payment Method:</label>
+          <select
+            required
+            className="input"
+            value={paymentMethod}
+            onChange={(e) => setPaymentMethod(e.target.value)}
+          >
+            <option value="">Select a Payment Method</option>
+            <option value="Credit Card">Credit Card</option>
+            <option value="Cash">Cash</option>
+            <option value="Bank Transfer">Bank Transfer</option>
+          </select>
+        </div>
+        <button type="submit" className="button">
+          Add Record
+        </button>
+      </form>
+    </div>
+  );
+};
 
-export default Form
+export default Form;
