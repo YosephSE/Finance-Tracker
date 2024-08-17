b7d9e262fdb64875b4372a5371d63b21619378d5
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-17 04:01:41 +0300

    controllers folder structure updated;

---
+++ server/src/controllers/record.ts
@@ -0,0 +1,59 @@
+import RecordModel from "../models/record";
+import { Request, Response } from "express";
+
+const editRecord = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const newRecord = req.body;
+    const saved = await RecordModel.findByIdAndUpdate(id, newRecord);
+
+    if (!saved) {
+      return res.status(404).json({ message: "Record Not Found" });
+    }
+    res.status(201).json(saved);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Error" });
+  }
+};
+
+const deleteRecord = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const saved = await RecordModel.findByIdAndDelete(id);
+    if (!saved) {
+      return res.status(404).json({ message: "Record Not Found" });
+    }
+    res.status(201).json(saved);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal Error" });
+  }
+};
+
+const addRecord = async (req: Request, res: Response) => {
+  try {
+    const newRecord = new RecordModel(req.body);
+    const saved = await newRecord.save();
+    res.status(201).json(saved);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal error" });
+  }
+};
+
+const getRecord = async (req: Request, res: Response) => {
+  try {
+    const userId: string = req.params.userId;
+    const records = await RecordModel.find({ userId });
+    if (records.length === 0) {
+      return res.status(404).json({ message: "No Record Found" });
+    }
+    return res.status(200).json(records);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Internal Error" });
+  }
+};
+
+export { getRecord, deleteRecord, addRecord, editRecord };
--- server/src/routes/record.ts
+++ server/src/routes/record.ts
@@ -1,59 +1,16 @@
-import { Router, Request, Response } from "express";
-import RecordModel from "../models/record";
-
+import { Router } from "express";
+import {
+  addRecord,
+  deleteRecord,
+  editRecord,
+  getRecord,
+} from "../controllers/record";
 const router: Router = Router();
 
-router.get("/:userId", async (req: Request, res: Response) => {
-  try {
-    const userId: string = req.params.userId;
-    const records = await RecordModel.find({ userId });
-    if (records.length === 0) {
-      return res.status(404).json({ message: "No Record Found" });
-    }
-    return res.status(200).json(records);
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({ message: "Internal Error" });
-  }
-});
-router.post("/", async (req: Request, res: Response) => {
-  try {
-    const newRecord = new RecordModel(req.body);
-    const saved = await newRecord.save();
-    res.status(201).json(saved);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Internal error" });
-  }
-});
+router.get("/:userId", getRecord);
+router.post("/", addRecord);
 
-router.put("/:id", async (req: Request, res: Response) => {
-  try {
-    const id = req.params.id;
-    const newRecord = req.body;
-    const saved = await RecordModel.findByIdAndUpdate(id, newRecord);
+router.put("/:id", editRecord);
 
-    if (!saved) {
-      return res.status(404).json({ message: "Record Not Found" });
-    }
-    res.status(201).json(saved);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Internal Error" });
-  }
-});
-
-router.delete("/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const saved = await RecordModel.findByIdAndDelete(id);
-    if (!saved) {
-      return res.status(404).json({ message: "Record Not Found" });
-    }
-    res.status(201).json(saved);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: "Internal Error" });
-  }
-});
+router.delete("/:id", deleteRecord);
 export default router;
