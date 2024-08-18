a587b3134a3457c29e72af1c066facec078d74b5
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-08-18 16:02:26 +0300

    cors added

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,15 @@ import express, { Express } from "express";
 import { connectDB } from "./config/db";
 import dotenv from "dotenv";
 import recordRouter from "./routes/record";
+import cors from "cors";
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
-app.use('/api', recordRouter)
+app.use("/api", recordRouter);
 
 connectDB();
 
